93e050b2d009685d2d4695503437796e2da4f4da
david.miles2 <david.miles2@sky.uk>
2026-01-02 11:51:54 +0000

    refactor/create app class

--- src/app.js
+++ src/app.js
@@ -1,51 +1,59 @@
 #!/usr/bin/env node
 const Auth = require('./auth.js');
-
 const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-console.log('Welcome to the Smart Home Automation App!');
-
-function mainMenu() {
-  rl.question(
-    `Select one of the following options:\n  1) Login\n  2) Exit\n> `,
-    (answer) => {
-      switch (answer.trim()) {
-        case '1':
-          login();
-          break;
-        case '2':
-          exit();
-          break;
-        default:
-          console.log('Invalid option, please try again.');
-          mainMenu();
-      }
-    }
-  );
-}
-
-function exit() {
-  console.log('Goodbye!');
-  rl.close();
-}
-function login() {
-  rl.question('Enter username: ', (username) => {
-    rl.question('Enter password: ', (password) => {
-      const isAuthenticated = Auth.validateCredentials(username, password);
-      if (isAuthenticated) {
-        console.log(`Login successful! Welcome, ${username}.`);
-        // todo: add logged in view
-      } else {
-        console.log('Invalid credentials. Please try again.');
-        mainMenu();
-      }
+class SmartHomeApp {
+  constructor() {
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
     });
-  });
+  }
+
+  start() {
+    console.log('Welcome to the Smart Home Automation App!');
+    this.mainMenu();
+  }
+
+  mainMenu() {
+    this.rl.question(
+      `Select one of the following options:\n  1) Login\n  2) Exit\n> `,
+      (answer) => {
+        switch (answer.trim()) {
+          case '1':
+            this.login();
+            break;
+          case '2':
+            this.exit();
+            break;
+          default:
+            console.log('Invalid option, please try again.');
+            this.mainMenu();
+        }
+      }
+    );
+  }
+
+  exit() {
+    console.log('Goodbye!');
+    this.rl.close();
+  }
+
+  login() {
+    this.rl.question('Enter username: ', (username) => {
+      this.rl.question('Enter password: ', (password) => {
+        const isAuthenticated = Auth.validateCredentials(username, password);
+        if (isAuthenticated) {
+          console.log(`Login successful! Welcome, ${username}.`);
+          // todo: add logged in view
+        } else {
+          console.log('Invalid credentials. Please try again.');
+          this.mainMenu();
+        }
+      });
+    });
+  }
 }
 
-mainMenu();
+const app = new SmartHomeApp();
+app.start();
