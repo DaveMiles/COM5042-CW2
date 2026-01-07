4aa761e30f1f87b625a30588418b418472d93e02
david.miles2 <david.miles2@sky.uk>
2026-01-07 19:58:43 +0000

    test/init device manager test suite

---
+++ test/deviceManager.test.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+const mockDevices = [
+  { id: 1, name: 'Lamp', type: 'Light', owner: 'user1' },
+  { id: 2, name: 'Thermostat', type: 'Climate', owner: 'user2' },
+  { id: 3, name: 'Camera', type: 'Security', owner: 'user1' },
+];
+
+jest.mock('fs');
+
+describe('DeviceManager', () => {
+  beforeEach(() => {
+    fs.readFileSync.mockImplementation((filePath, encoding) => {
+      if (filePath.includes('devices.json')) {
+        return JSON.stringify(mockDevices);
+      }
+      return '';
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it.todo('should load devices for the specified user', () => {});
+
+  it.todo('should not load devices for other users', () => {});
+
+  it.todo('should list devices in the correct format', () => {});
+});
