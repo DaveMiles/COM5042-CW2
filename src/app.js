5e0a8eed5345eaed4ddbdf843f94dae46f49c37d
Jgrantawish <joannaindiagrant@gmail.com>
2026-01-10 18:55:54 +0000

    feat/create new user accounts

--- src/app.js
+++ src/app.js
@@ -18,13 +18,16 @@ class SmartHomeApp {
 
   mainMenu() {
     this.rl.question(
-      `Select one of the following options:\n  1) Login\n  2) Exit\n> `,
+      `Select one of the following options:\n  1) Login\n  2) Create account\n  3) Exit\n> `,
       (answer) => {
         switch (answer.trim()) {
           case '1':
             this.login();
             break;
           case '2':
+            this.createAccount();
+          break;
+          case '3':
             this.exit();
             break;
           default:
@@ -39,6 +42,35 @@ class SmartHomeApp {
     console.log('Goodbye!');
     this.rl.close();
   }
+
+  async createAccount(){
+    const name = await this.getVaildUserInput('Enter your name: ');
+    const username = await this.getVaildUserInput('Create a username (between ' + Auth.minInputLength + ' and ' + Auth.maxInputLength + ' characters): ');
+    const password = await this.getVaildUserInput('Create a password (between ' + Auth.minInputLength + ' and ' + Auth.maxInputLength + ' characters): ');
+  
+    Auth.addUserAccount(name, username, password);
+    
+    this.mainMenu();
+  }
+
+  async getVaildUserInput(prompt){
+    let input = null;
+
+    while (input == null){
+      const userAnswer = await new Promise(resolve =>
+        this.rl.question(prompt, resolve)
+      );
+      input = Auth.sanitiseInput(userAnswer);
+
+      if (input == null) {
+        console.log('Invalid input, please try again.');
+      }
+
+    };
+
+    return input;
+  }
+
 
   login() {
     this.rl.question('Enter username: ', (username) => {
--- src/auth.js
+++ src/auth.js
@@ -1,8 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const usersPath = path.join(__dirname, '../database/users.json');
+
 class Auth {
   constructor() {
-    const fs = require('fs');
-    const path = require('path');
-    const usersPath = path.join(__dirname, '../database/users.json');
     this.users = JSON.parse(fs.readFileSync(usersPath, 'utf8'));
     this.minInputLength = 1;
     this.maxInputLength = 20;
@@ -33,13 +34,41 @@ class Auth {
     }
 
     return null;
-  
+    
   }
 
   hashPassword(password) {
     const crypto = require('crypto')
     
     return crypto.createHash('sha256').update(password).digest('hex');
+  }
+
+  addUserAccount(name, username, password){
+    try {
+      const id = this.getNextUserId();
+      const hashedPassword = this.hashPassword(password);
+      this.users.push({
+        "id": id,
+        "displayName": name,
+        "username": username,
+        "passwordHash": hashedPassword
+      });
+
+      const userJsonString = JSON.stringify(this.users);
+
+      fs.writeFileSync(usersPath, userJsonString, 'utf-8');
+
+      console.log('Account sucessfully created!');
+
+    } catch (err) {
+      console.error('Failed to save user:', err);
+    }
+  }
+
+  getNextUserId() {
+    if (this.users.length === 0) return 0;
+
+    return Math.max(...this.users.map(user => user.id)) + 1;
   }
 
 }
